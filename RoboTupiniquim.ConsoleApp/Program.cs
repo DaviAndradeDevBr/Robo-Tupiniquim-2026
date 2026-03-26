409fe98dbc4c19381265a55e230a50048b2e4b0e
Davi Andrade <contatodaviandrade20@gmail.com>
2026-03-26 20:11:43 -0300

    Adicionada lógica de rotação do robô

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -1,9 +1,57 @@
 ﻿namespace RoboTupiniquim.ConsoleApp;
 
+/*
+    ● Posição e Orientação: A posição do robô é dada por coordenadas (X, Y) e uma
+    letra que representa a direção para onde ele está olhando (Norte, Sul, Leste, Oeste).
+    
+    ● Comandos: A AEB envia strings de comando simples (E, D, M):
+        1. E (Esquerda) e D (Direita) fazem o robô virar 90 graus, sem sair do lugar.
+        2. M (Mover) move o robô uma posição no grid para frente, mantendo a direção.
+
+*/
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int posicaoX = 1;
+        int posicaoY = 2;
+        char orientacao = 'N';
+
+        string comandoCompleto = "EMEMEMEMM";
+        
+        
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("Robô Tupiniquim");
+        Console.WriteLine("-------------------------------");
+        
+        Console.WriteLine($"Posição inicial: {posicaoX} {posicaoY} {orientacao}");
+
+        // algoritmo
+        for (int contador = 0; contador < comandoCompleto.Length; contador++)
+        {
+            char comandoAtual = comandoCompleto[contador];
+
+            Console.WriteLine(comandoAtual);
+
+            if (comandoAtual == 'E')
+            {
+                if (orientacao == 'N') orientacao = 'O';
+                else if (orientacao == 'O') orientacao = 'S';
+                else if (orientacao == 'S') orientacao = 'L';
+                else if (orientacao == 'L') orientacao = 'N';
+            }
+            else if (comandoAtual == 'D')
+            {
+                if (orientacao == 'N') orientacao = 'L';
+                else if (orientacao == 'L') orientacao = 'S';
+                else if (orientacao == 'S') orientacao = 'O';
+                else if (orientacao == 'O') orientacao = 'N';
+            }
+        }
+        
+        Console.WriteLine($"Posição final: {posicaoX} {posicaoY} {orientacao}");
+        Console.ReadLine();
+
     }
 }
